1e54768929e2de02957666834e3fa09230d6ec76
huharvey <huharveyy@gmail.com>
2025-10-24 22:50:22 +0800

    updated

--- Core/Src/gm6020.cpp
+++ Core/Src/gm6020.cpp
@@ -2,7 +2,7 @@
  * @Author: huharvey huharveyy@gmail.com
  * @Date: 2025-10-20 17:44:07
  * @LastEditors: huharvey huharveyy@gmail.com
- * @LastEditTime: 2025-10-20 19:15:13
+ * @LastEditTime: 2025-10-24 22:49:30
  * @FilePath: \hw_2\Core\Src\gm6020.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
